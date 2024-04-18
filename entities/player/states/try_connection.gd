b39fc7fe30ffbb6394ed1c2568788a04db76bd8a
FrancoYudica <francoyudica@gmail.com>
2024-04-18 16:41:58 -0300

    Obstacle material and Hook intersects when entered in path

--- entities/player/states/try_connection.gd
+++ entities/player/states/try_connection.gd
@@ -29,9 +29,10 @@ func _state_process(delta):
 		
 	var hookable = player.raycast.intersecting_hookable
 		
-	if hookable != null and (player.hook.head.global_position - hookable.get_intersection_point()).length() < hook_pixel_grab_edge:
+	#if hookable != null and (player.hook.head.global_position - hookable.get_intersection_point()).length() < hook_pixel_grab_edge:
+	#	_on_hookable_hooked(hookable)
+	if hookable != null and (player.hook.head.global_position - hookable.get_intersection_point()).length() < _hook_length:
 		_on_hookable_hooked(hookable)
-		
 		
 func _on_hookable_hooked(hookable: Hookable):
 	
